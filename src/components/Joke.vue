b65ecca930ed4039b72b087d239179de44a81c94
Chandrima Barua <chandrimabarua62065@gmail.com>
2020-06-28 10:32:42 +0600

    initial commit

---
+++ src/components/Joke.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col-md-6">
+    <div class="joke-card">
+    <div class="card-block">
+    <button class="close" @click="removeJoke(index)"><span>&times;</span></button>
+    <p><u>{{jokes.setup}}</u></p>
+    <p><em>{{jokes.punchline}}</em></p>
+    </div>
+    </div>
+    </div>
+</template>
+<script>
+import {mapActions} from 'vuex'
+
+export default {
+    props:['jokes', 'index'],
+    methods:mapActions([
+        
+        'removeJoke'
+        
+    ]),
+}
+</script>
